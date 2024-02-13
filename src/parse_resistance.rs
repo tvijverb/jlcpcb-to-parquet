3f261a269aa519314ad5f29565ab28d76e9b5712
tvijverb <thomas@datastem.nl>
2024-02-14 00:49:03 +0100

    update resistance calc for bigger ohm values (>8 Mohm)

--- src/parse_resistance.rs
+++ src/parse_resistance.rs
@@ -6,44 +6,44 @@ pub fn parse_resistance_description(component_description: &str) -> Option<f64> 
         if part.ends_with("pΩ") {
             let numeric_part = part.trim_end_matches("pΩ");
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value);
+                resistance_value = Some(value* 1e-12);
                 break;
             }
         } 
         else if part.ends_with("nΩ") {
             let numeric_part = part.trim_end_matches("nΩ");
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value*1_000.0);
+                resistance_value = Some(value*1e-9);
                 break;
             }
         } else if part.ends_with("uΩ") {
             let numeric_part = part.trim_end_matches("uΩ");
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value*1_000_000.0);
+                resistance_value = Some(value*1e-6);
                 break;
             }
         } else if part.ends_with("mΩ") {
             let numeric_part = part.trim_end_matches("mΩ");
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value*1_000_000_000.0);
+                resistance_value = Some(value*1e-3);
                 break;
             }
         } else if part.ends_with("kΩ") {
             let numeric_part = part.trim_end_matches("kΩ");
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value*1_000_000_000_000_000.0);
+                resistance_value = Some(value*1e3);
                 break;
             }
         } else if part.ends_with("MΩ") {
             let numeric_part = part.trim_end_matches("MΩ");
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value*1_000_000_000_000_000_000.0);
+                resistance_value = Some(value*1e6);
                 break;
             }
         } else if part.ends_with('Ω') {
             let numeric_part = part.trim_end_matches('Ω');
             if let Ok(value) = numeric_part.parse::<f64>() {
-                resistance_value = Some(value*1_000_000_000_000.0);
+                resistance_value = Some(value);
                 break;
             }
         } 
